10a016c5ecf6052f798ab09659f98b4e6a8aa6f4
Chelsea Du <chsea.du@gmail.com>
2015-10-01 14:45:35 -0400

    cards routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,8 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/games', require('./games'));
+router.use('/cards', require('./games'));
 
 // Make sure this is after all of
 // the registered routes!
