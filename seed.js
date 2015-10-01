ac822f500430cc1bd6cff7c306f84b27ac906f74
Kate <katemcdonald39@gmail.com>
2015-10-01 18:45:59 -0400

    fixed bug in seed. Seed was failing because of validation for minion. Added newly required properties

--- seed.js
+++ seed.js
@@ -73,30 +73,11 @@ var seedUsers = function () {
 };
 
 function seedMinions() {
-  var minions = [
-    {
-      name: 'Uber',
-      category: 'transportation',
-      cost: 10,
-      hitPoints: 7,
-      attackPoints: 10,
-      description: 'Uber cool!',
-      rarity: 3,
-      portrait: 'http://thecatapi.com/api/images/get?format=src&type=gif',
-    },
-    {
-      name: 'Slack',
-      category: 'communication',
-      cost: 6,
-      hitPoints: 4,
-      attackPoints: 7,
-      description: 'Slack!',
-      rarity: 1,
-      portrait: 'http://thecatapi.com/api/images/get?format=src&type=gif',
-    }];
+  var minions = [];
 
     for (var i = 0; i < 15; i++){
         var obj = {};
+        obj.type = "minion";
         obj.name = companies[Math.floor(Math.random() * companies.length)];
         obj.category = category[Math.floor(Math.random() * category.length)];
         obj.cost = Math.floor(Math.random() * 10);
@@ -105,6 +86,8 @@ function seedMinions() {
         obj.description = "Y Combinator Company";
         obj.rarity = Math.floor(Math.random() * 4);
         obj.portrait = "http://thecatapi.com/api/images/get?format=src&type=gif";
+        obj.hp = Math.floor(Math.random() * 10);
+        obj.ap =  Math.floor(Math.random() * 4);
         minions.push(obj);
     }
     console.log(minions);
@@ -115,6 +98,7 @@ function seedMinions() {
 function seedSpells() {
   var spells = [
     {
+      type: "spell",
       name: 'Uber',
       category: 'transportation',
       cost: 10,
@@ -122,7 +106,7 @@ function seedSpells() {
       attackPoints: 10,
       description: 'Uber cool!',
       rarity: 3,
-      portrait: '/images/uber.jpg',
+      portrait: 'http://thecatapi.com/api/images/get?format=src&type=gif',
     }
   ];
 
