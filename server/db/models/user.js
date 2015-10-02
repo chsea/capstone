300528c689aeede8364fbb77f717077675b6caec
alex <alexisriverox@gmail.com>
2015-10-02 16:08:28 -0400

    added deck model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,51 +6,50 @@ var deepPopulate = require('mongoose-deep-populate')(mongoose)
 
 
 var schema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    password: {
-        type: String
-    },
-    salt: {
-        type: String
-    },
-    google: {
-        id: String
-    },
-    photo: {
-        type: String,
-        default: '/images/default-img.png'
-    },
-    cards: [{
-        type: ObjectId,
-        ref: "Cards"
-    }],
-    decks: [{
-      name: String,
-      cards: [{type: ObjectId, ref: "Cards"}]
-    }],
-
-    experience:{
-      type:Number,
-      max: 100,
-      default:0
-    },
-    level: {
-        type: Number,
-        max: 50,
-        default: 0
-    }
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  password: {
+    type: String
+  },
+  salt: {
+    type: String
+  },
+  google: {
+    id: String
+  },
+  photo: {
+    type: String,
+    default: '/images/default-img.png'
+  },
+  cards: [{
+    type: ObjectId,
+    ref: "Cards"
+  }],
+  decks: {
+    type: ObjectId,
+    ref: 'Decks'
+  },
+  experience: {
+    type: Number,
+    max: 100,
+    default: 0
+  },
+  level: {
+    type: Number,
+    max: 50,
+    default: 0
+  }
 });
 
 
@@ -64,9 +63,9 @@ schema.plugin(deepPopulate, {});
 // generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
 // are all used for local authentication security.
 
-schema.method('experienceToLevel', function(){
-  if(this.experience >= 100) {
-    this.level +=1
+schema.method('experienceToLevel', function() {
+  if (this.experience >= 100) {
+    this.level += 1
     this.experience = this.experience % 100;
   }
 })
@@ -74,36 +73,36 @@ schema.method('experienceToLevel', function(){
 
 
 
-var generateSalt = function () {
-    return crypto.randomBytes(16).toString('base64');
+var generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
 };
 
-var encryptPassword = function (plainText, salt) {
-    var hash = crypto.createHash('sha1');
-    hash.update(plainText);
-    hash.update(salt);
-    return hash.digest('hex');
+var encryptPassword = function(plainText, salt) {
+  var hash = crypto.createHash('sha1');
+  hash.update(plainText);
+  hash.update(salt);
+  return hash.digest('hex');
 };
 
-schema.pre('save', function (next) {
+schema.pre('save', function(next) {
 
-    if (this.isModified('password')) {
-        this.salt = this.constructor.generateSalt();
-        this.password = this.constructor.encryptPassword(this.password, this.salt);
-    }
-    if(this.experience >100) {
-      this.level +=1
-      this.experience = 0;
-    }
-    next();
+  if (this.isModified('password')) {
+    this.salt = this.constructor.generateSalt();
+    this.password = this.constructor.encryptPassword(this.password, this.salt);
+  }
+  if (this.experience > 100) {
+    this.level += 1
+    this.experience = 0;
+  }
+  next();
 
 });
 
 schema.statics.generateSalt = generateSalt;
 schema.statics.encryptPassword = encryptPassword;
 
-schema.method('correctPassword', function (candidatePassword) {
-    return encryptPassword(candidatePassword, this.salt) === this.password;
+schema.method('correctPassword', function(candidatePassword) {
+  return encryptPassword(candidatePassword, this.salt) === this.password;
 });
 
 mongoose.model('User', schema);
