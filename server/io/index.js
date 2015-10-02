c4240bf84f3f8a4af950c3599b16097f2920524d
Chelsea Du <chsea.du@gmail.com>
2015-10-02 11:13:52 -0400

    automatic join games

--- server/io/index.js
+++ server/io/index.js
@@ -7,44 +7,45 @@ module.exports = function (server) {
 
   io = socketio(server);
 
-  let avalibleUsers = [];
-  let pendingUsers = [];
-  let avalibleGames = [];
-  let pendingGames = [];
+  // let availableUsers = [];
+  // let pendingUsers = [];
+  // let availableGames = [];
+  // let pendingGames = [];
 
   let waitingPlayers = [];
   io.on('connection', socket => {
     console.log('user connected');
 
-    socket.on('enterCreateRoom', user => {
-      if (!_.find(avalibleUsers, { _id: user._id}) && !_.find(pendingUsers, { _id: user._id})) avalibleUsers.push(user);
-      io.emit('newUser', avalibleUsers, avalibleGames);
-    });
-
-    socket.on('joinGame', (i, user) => {
-      if (!avalibleGames[i]) avalibleGames.push({player1: user});
-      else {
-        avalibleGames[i].player2 = user;
-        let newGame = avalibleGames.splice(i, 1)[0];
-        pendingGames.push(newGame);
-      }
-      _.remove(avalibleUsers, avalibleUser => avalibleUser._id == user._id);
-      pendingUsers.push(user);
-      io.emit('gameJoined', avalibleUsers, avalibleGames);
-    });
+    // socket.on('enterCreateRoom', user => {
+    //   if (!_.find(availableUsers, { _id: user._id}) && !_.find(pendingUsers, { _id: user._id})) availableUsers.push(user);
+    //   io.emit('newUser', availableUsers, availableGames);
+    // });
+    //
+    // socket.on('joinGame', (i, user) => {
+    //   if (!availableGames[i]) availableGames.push({player1: user});
+    //   else {
+    //     availableGames[i].player2 = user;
+    //     let newGame = availableGames.splice(i, 1)[0];
+    //     pendingGames.push(newGame);
+    //   }
+    //   _.remove(availableUsers, availableUser => availableUser._id == user._id);
+    //   pendingUsers.push(user);
+    //   io.emit('gameJoined', availableUsers, availableGames);
+    // });
 
     socket.on('join', (player, deck) => {
       if (_.find(waitingPlayers, {player: {_id: player._id}})) return;
 
-      let matchingPlayers = waitingPlayers.map((player, i) => {
-        return {player: player, index: i};
-      }).filter(matchingPlayer => matchingPlayer.player._id === player._id);
+      // let matchingPlayers = waitingPlayers.filter(matchingPlayer => matchingPlayer.player._id === player._id);
+      let matchingPlayers = waitingPlayers;
       if (matchingPlayers.length) {
-        waitingPlayers.splice(matchingPlayers[0].index, 1);
-        Socket.emit('startGame');
+        let player1 = waitingPlayers.splice(matchingPlayers[0].index, 1)[0];
+        player1.socket.emit('startGame');
+        console.log('hi');
+        socket.emit('startGame');
       }
       else {
-        waitingPlayers.push({player: player, deck: deck});
+        waitingPlayers.push({player: player, deck: deck, socket: socket, index: waitingPlayers.length});
         socket.emit('waitForPlayer');
       }
     });
