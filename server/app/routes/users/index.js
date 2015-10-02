c19fb8de288d08c86ac8fb6de2d897a19ea5dba1
Kate <katemcdonald39@gmail.com>
2015-10-01 20:18:52 -0400

    added require mongoose statement so the app will run

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -1,7 +1,7 @@
 'use strict';
+var mongoose = require('mongoose');
 var router = require('express').Router();
 module.exports = router;
-var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 
