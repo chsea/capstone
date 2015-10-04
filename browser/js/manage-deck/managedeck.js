f1c4a29c73fb78e204cd69d69efacce47bac517f
Kate <katemcdonald39@gmail.com>
2015-10-04 19:53:17 -0400

    calculated total cost of a deck

--- browser/js/manage-deck/managedeck.js
+++ browser/js/manage-deck/managedeck.js
@@ -10,11 +10,29 @@ app.config(function($stateProvider) {
 });
 
 app.controller('manageDeckController', function($scope, user) {
+  $scope.total = 0;
+  $scope.cost = 0;
+  // caluclaing cost of total mana
+  $scope.labels = ['0', '1', '2', '3', '4', '5', '6','7+'];
+  $scope.series;
+  $scope.showCards = false;
+  $scope.cards = [];
   $scope.decks = user.decks;
   $scope.selectDeck = () => {
-    $scope.cards = _.find(user.decks, {_id: $scope.selectedDeck}).cards;
+    $scope.deck = _.find(user.decks, {_id: $scope.selectedDeck});
+    $scope.cards = $scope.deck.cards;
+    $scope.total = $scope.cards.length;
+    $scope.series = $scope.deck.name;
+    $scope.showCards = true;
   };
-    // $scope.userCards = user.cards
-    // $scope.currentDeck = user.decks
+  
+  $scope.calccost = function() {
+    $scope.data = [];
+    $scope.cards.forEach(function(card) {
+      $scope.data.push(card.cost);
+    });
+    return $scope.data;
+  };
 
+ 
 });
