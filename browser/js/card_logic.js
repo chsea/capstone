b4a7c5baa9c832befaf02d6fc29a798e0391cf63
alex <alex>
2015-10-04 22:52:04 -0400

    lol

---
+++ browser/js/card_logic.js
@@ -0,0 +1,138 @@
+// general
+
+function balancedTeam(){
+  // if you have three differnet types of units
+  //raise their attacks by 20% increase by 20% for each differnet unit
+}
+
+//start up abilities
+function shelter (){
+  //airbnb
+  // selected unit takes 50% less damage
+
+}
+function getAround (){
+  //uber
+  // selected unit can attack twice this turn
+
+}
+
+function communication (){
+  //slack
+  // can only use if 3 or more units on the field,
+  // allows you to act as if the units combined to be one unit
+}
+
+function disappear (){
+  //snapcaht
+  // select a unit, it disappears in 3 turns
+
+}
+function StudentOrGraduate(){
+  //FullstackAcademy
+  // summon a student, or graduate a student
+
+}
+
+function encryption(){
+  //coinbase
+  //encrypt your units, give them all 1 armor
+}
+
+function browse(){
+  //pinterest
+  //randomly add a card from your deck into your hand
+}
+
+
+
+
+
+//spell cards
+function poachEmployee(){}
+
+function techBoom() {
+    // raise all valuations by 50%
+}
+
+function nasdaqCrash (){
+  // lower all valuations by 50%
+
+}
+function downRound(){
+  //lower enemies valuations by 20%
+
+
+}
+
+function IPO (){
+  // cash in your startup, empowering ur units and doubling
+  // valuation for next 3 turns
+
+
+}
+
+function agileMethodology(){
+  // use agile metholody, allows units to attack twice this turn
+
+
+}
+
+function consultant () {
+  //hire a consultant
+}
+
+
+
+// minion- special abilities
+
+//passive special abiltiies
+function passive(){
+
+
+}
+// initiave same as charge in hearthstone
+function initiative(){
+
+}
+// Agile same as windury
+// windfury
+function Agile (){
+
+}
+
+
+
+// inspiration same as battlecry in hearthstone
+// things taht happen on summon
+function inspiration(){
+  function davidOrNimit(){
+    //summon a fullstackGraduate
+    //if both are on the field summon a fullstack graduate everyTurn
+    // doubles each other attack and health
+  }
+  function MarkZuckerburg(){
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
---
+++ cards.js
@@ -0,0 +1,218 @@
+var spellCards = [
+  //name is name that appaers on view
+  // logic is just a string that represents a function
+  {
+    name: 'Tech Boom',
+    logic: 'techBoom',
+    description: 'TechBoom is in town! Increase all valuations by 50%',
+    rarity: 's',
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }, {
+    name: 'Nasdaq Crash',
+    logic: 'nasdaqCrash',
+
+    description: 'tech companies are crashing! run! Cut all valuations by 50%',
+
+    rarity: 3,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }, {
+    name: 'Agile Methodology',
+    logic: 'agileMethodology',
+
+    description: 'It\'s all about the burn-down chart! Units can attack twice this turn',
+    rarity: 3,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }, {
+    name: 'Down Round',
+    logic: 'downRound',
+
+    description: 'Things aren\'t going so well. Lower your opponents valuation by 30%',
+    rarity: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }, {
+    name: 'Consultant',
+    logic: 'consultant',
+
+    description: 'Need some outside help. For the next three turns there are 2 more minions on the field',
+    rarity: 5,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }, {
+    name: 'IPO',
+    logic: 'da',
+
+    description: 'Lets get rich (on paper)! Increases valuation and unit health and attack by 50% for next three turns only',
+    rarity: 's',
+    cost: 2,
+    portait: 'a',
+    category: 's'
+
+  }
+
+
+]
+
+
+
+
+var minionCards = [
+  {
+    name: 'David Yang',
+    logic: 'davidOrNimit',
+    description: 'A Legendary Co-Founder. Inspiration:Summons a Fullstack Gradute; 3 if Nimit is around',
+    rarity: 3,
+    health: 8,
+    attack: 10,
+    cost: 7,
+    portait: 'a',
+    category: 'founder'
+  },
+  {
+    name: 'Nimit Maaru',
+    logic: 'davidOrNimit',
+    description: 'A Legendary Co-Founder. Inspiration:Summons a Fullstack Gradute; 3 if David is around',
+    rarity: 3,
+    health: 10,
+    attack: 8,
+    cost: 7,
+    portait: 'a',
+    category: 'founder'
+  },{
+    name: 'Mark Zuckerburg',
+    logic: 'Mark',
+    description: 'A Legendary, young CEO-Founder. Inspiration: doubles all technical units attack for two turns',
+    rarity: 3,
+    health: 7,
+    attack: 7,
+    cost: 6,
+    portait: 'a',
+    category: 'founder'
+  },{
+    name: 'Fullstack Graduate',
+    logic: 2,
+    description: 'A truly great engineer. Gains +1 attack and strength for each other Fullstack graduate, gains +2 with Nimit or David',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Junior Front-End Enngineer',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Junior Back-End Engineer',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Senior Front-end Engineer',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Senior Back-end Engineer',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: '',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Fullstack Graduate',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Harvard MBA',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'AWS Ninja',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Fullstack Graduate',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },{
+    name: 'Fullstack Graduate',
+    logic: 'da',
+    description: '',
+    rarity: 's',
+    health: 2,
+    attack: 4,
+    cost: 2,
+    portait: 'a',
+    category: 's'
+  },
+
+
+
+]
