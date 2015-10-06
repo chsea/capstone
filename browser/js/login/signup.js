311397220d33093a145e686fe780f709c65bb84d
Alex <alexisriverox@gmail.com>
2015-10-06 14:10:01 -0400

    signup form

--- browser/js/login/signup.js
+++ browser/js/login/signup.js
@@ -6,12 +6,42 @@ app.config(function($stateProvider) {
   })
 })
 
-app.controller('signupCtrl', function($scope, AuthService, UserFactory) {
+app.controller('signupCtrl', function($scope, $state, AuthService, UserFactory) {
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  $scope.emailValidate = function(email) {
+    return re.test(email)
+  }
 
-   $scope.signup = function(){
-
-   }
 
 
+
+  $scope.$emailValid = true;
+
+  function resetUser() {
+    $scope.newUser = {
+      email: null,
+      username: null,
+      password: null
+    }
+  }
+
+  $scope.signup = function() {
+    if (!$scope.emailValidate($scope.newUser.email)) {
+      $scope.$emailValid = false
+      console.log('lol')
+    } else {
+      UserFactory.create($scope.newUser)
+        .then(user => {
+          console.log(user)
+          resetUser()
+          $state.go('login')
+        })
+    }
+  }
+
+
+
+
+  resetUser()
 
 })
