9a83188e5f06001fb5b31e43bca2c200e52b913b
Kate <katemcdonald39@gmail.com>
2015-10-07 23:20:39 -0400

    refactoring managedeck state to make the front end bound to the back end in real time

--- browser/js/manage-deck/managedeck.js
+++ browser/js/manage-deck/managedeck.js
@@ -12,27 +12,21 @@ app.config(function($stateProvider) {
 app.controller('manageDeckController', function($scope, user, $http, $state, DeckFactory, UserFactory) {
   var context;
   var clientsChart;
-  $scope.total = 0;
   $scope.barData = {};
   $scope.showCards = false;
   $scope.createDeck = false;
+  $scope.cardsInDeck = {};
+  $scope.user = user;
   $scope.craft = false;
-  $scope.uniqueCardsInDeck = {};
-  $scope.username = user.username;
-  $scope.decks = user.decks;
-  $scope.usercards = user.cards;
-  $scope.selectDeck = () => {
-    if ($scope.selectedDeck === undefined) return;
-    $scope.deck = _.find(user.decks, {_id: $scope.selectedDeck});
-    $scope.deck.cards.forEach(function(card){
-      if ($scope.uniqueCardsInDeck.hasOwnProperty(card.name)){
-        $scope.uniqueCardsInDeck[card.name] += 1;
-      } else {
-        $scope.uniqueCardsInDeck[card.name] = 1;
+  
+  $scope.selectDeck = function() {
+    $scope.currentdeck = _.find($scope.user.decks, {_id: $scope.selectedDeck});
+      if ($scope.selectedDeck === undefined) {
+        $scope.currentdeck = $scope.user.decks[0];
+        $scope.selectedDeck = $scope.user.decks[0];
       }
-    });
-    $scope.deckName = $scope.deck.name;
-    $scope.total = $scope.deck.cards.length;
+    $scope.cardsInDeck = displayDeck();
+    $scope.deckName = $scope.currentdeck.name;
     $scope.showCards = true;
     $scope.barData.labels = ["1", "2", "3", "4", "5", "6","7+"];
     $scope.barData.datasets = [{
@@ -47,10 +41,23 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
   context = document.getElementById('clients').getContext('2d');
   clientsChart = new Chart(context).Bar($scope.barData);
   };
+
+  function displayDeck() {
+    if ($scope.currentdeck === undefined) return;
+    var cardsInDeckObj = {};
+    $scope.currentdeck.cards.forEach(function(card){
+      if (cardsInDeckObj.hasOwnProperty(card.name)){
+        cardsInDeckObj[card.name] += 1;
+      } else {
+        cardsInDeckObj[card.name] = 1;
+      }
+    });
+    return cardsInDeckObj;
+  };
   
   $scope.deckcost = function() {
     $scope.cost = [0,0,0,0,0,0,0];
-    $scope.deck.cards.forEach(function(card) {
+    $scope.currentdeck.cards.forEach(function(card) {
       if (card.cost <= 6) {
         $scope.cost[card.cost-1]++;
       }
@@ -62,23 +69,29 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
   };
 
   $scope.removeFromDeck = function(cardname) {
-    if ($scope.total < 1 || $scope.deck === undefined) return;
-    $scope.deck.cards.forEach(function(currentcard, index) {
+    if ($scope.total < 1 || $scope.currentdeck === undefined) return;
+    var indx = -1;
+    $scope.currentdeck.cards.forEach(function(currentcard, index) {
       if (currentcard.name === cardname) {
-        $scope.deck.cards.splice(index, 1);
-        return;
+        indx = index;
       }
-      updateDeck($scope.deck);
     });
+    $scope.currentdeck.cards.splice(indx, 1);
+    updateDeck();
   };
 
   $scope.removeDeck = function() {
-    if ($scope.deck === undefined) return;
-    DeckFactory.destroy($scope.deck._id)
+    if ($scope.currentdeck === undefined) return;
+    DeckFactory.destroy($scope.currentdeck._id)
     .then(function(deletedDeck){
-      //console.log("deleted deck", deletedDeck);
-    }).then(null, function(err){
-      console.log(err);
+      var indx = -1;
+      $scope.user.decks.forEach(function(thisdeck, index){
+        if (thisdeck._id === deletedDeck._id){
+          indx = index;
+        }
+      });
+      $scope.user.decks.splice(indx, 1);
+      updateUser();
     });
   };
 
@@ -94,20 +107,19 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
     if (deckname === undefined || deckname.length < 1) return;
     DeckFactory.create({"name": deckname})
     .then(function(newdeck){
-      $scope.decks.push(newdeck);
-      user.decks = $scope.decks;
+      $scope.user.decks.push(newdeck);
       updateUser();
     });
   };
 
   $scope.addToDeck = function(card){
-    if ($scope.deck === undefined || $scope.total > 29) return;
+    if ($scope.currentdeck === undefined || $scope.total > 29) return;
     if (duplicateChecker(card)) {
-      console.log("cannot have more than 2 duplicates in the deck");
+      console.log("cannot have more than 2 duplicates in the currentdeck");
       return;
     }
-    $scope.deck.cards.push(card);
-    updateDeck($scope.deck);
+    $scope.currentdeck.cards.push(card);
+    updateDeck();
   };
 
   $scope.showCraftForm = function() {
@@ -123,14 +135,15 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
     var cost = card.stardustCost;
     removeCardFromUser(card);
     // 2. Add card's stardust value to user's stardust points
-    user.stardust += cost;
+    $scope.user.stardust += cost;
     updateUser();
   };
 
-  function updateDeck(deck){
-    DeckFactory.update(deck._id, deck)
+  function updateDeck(){
+    DeckFactory.update($scope.currentdeck._id, $scope.currentdeck)
       .then(function(updatedDeck){
-        //console.log("deck updated");
+        $scope.cardsInDeck = displayDeck();
+        $scope.currentdeck = updatedDeck;
       })
       .then(null, function(err){
         console.log("Error occured ", err);
@@ -138,20 +151,20 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
   }
 
   function updateUser(){
-    UserFactory.update(user._id, user)
+    UserFactory.update($scope.user._id, $scope.user)
       .then(function(updatedUser){
-        // console.log("user updated");
-        return true;
+        console.log("updated user ", updatedUser);
+        user = updatedUser;
+        $scope.user = updatedUser;
       })
       .then(null, function(err){
         console.log("Error occured ", err);
-        return false;
       });
   }
 
   function duplicateChecker(card) {
     var count = 0;
-    $scope.deck.cards.forEach(function(currentcard){
+    $scope.currentdeck.cards.forEach(function(currentcard){
       if (currentcard._id === card._id){
         count++;
       }
@@ -163,12 +176,12 @@ app.controller('manageDeckController', function($scope, user, $http, $state, Dec
   function removeCardFromUser(card) {
     // remove the card from the user's user.cards property
     var indx = -1;
-    user.cards.forEach(function(currentcard, index){
+    $scope.user.cards.forEach(function(currentcard, index){
       if (currentcard._id === card._id){
         indx = index;
       }
     });
-    user.cards.splice(indx, 1);
+    $scope.user.cards.splice(indx, 1);
   }
 
 });
