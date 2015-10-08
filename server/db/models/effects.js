62276edea04bd2e72c23460e990346a7fc56edfb
Alex <alexisriverox@gmail.com>
2015-10-08 14:28:12 -0400

    some cards

--- server/db/models/effects.js
+++ server/db/models/effects.js
@@ -11,9 +11,7 @@ var effectsSchema = new mongoose.Schema({
   },
   target: {
     type: [String],
-    enum: ['player', 'playerMinion','opponentMinion', 'opponent'],
-
-
+    enum: ['player', 'playerCard','opponentCard', 'opponent','anyCard'],
   },
   quantity: {
     type: Number
@@ -36,14 +34,20 @@ var healSchema = effectsSchema.extend({
 });
 
 var AlterPropertySchema = effectsSchema.extend({
-  AP: {
+  attackPoints: {
     type: Number
   },
-  HP:{
+  hitPoints:{
     type: Number
   } ,
-  lowerCost: {
+  cost: {
     type: Number
+  },
+  logic:{
+    windury: Boolean,
+    battlecry: Boolean,
+    divineShield: Boolean,
+    charge: Boolean
   }
 });
 
