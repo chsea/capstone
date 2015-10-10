927f3a6d00dec1ddfd478e91318bd3b66779c381
Kate <katemcdonald39@gmail.com>
2015-10-10 15:02:08 -0400

    merged lobby and home into a single state

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,65 @@
 app.config(function ($stateProvider) {
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'js/home/home.html'
+  $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'js/home/home.html',
+      controller: 'LandingController',
+      resolve: {
+				user: function(AuthService) {
+					return AuthService.getLoggedInUser();
+				}
+			},
+			data: {
+				authenticate: true
+			}
     });
+});
+
+app.controller('LandingController', function($scope, user, UserFactory,CardFactory) {
+
+  $scope.showPack = false;
+  $scope.showCards = [];
+	$scope.uiRouterState = "home";
+
+  $scope.openPack = function(){
+    console.log('clicked openpack');
+    if (user.packs < 1 ) return;
+    user.packs -= 1;
+    
+    CardFactory.findAll().then(cards => {
+      let chosenCards = _.sample(cards,5);
+      $scope.showCards = chosenCards;
+      $scope.showPack = true;
+      return chosenCards;
+    }).then(theCards => {
+      var cardIds = [];
+      theCards.forEach(card => {
+        cardIds.push(card._id);
+      });
+      user.cards = user.cards.concat(cardIds);
+      return UserFactory.update(user , {cards: user.cards, packs: user.packs   , isAdmin:false});
+
+    }).then(function(user){
+
+    });
+  };
+
+  $scope.user = user;
+  $scope.options = [{
+    name: 'Play Now',
+    sref: 'play'
+  }, {
+    name: 'Manage Deck',
+    sref: 'manageDeck'
+  }, {
+    name: 'Join Game',
+    sref: 'joinGame'
+  }, {
+    name: 'Create Game',
+    sref: 'joinGame'
+
+  }, {
+    name: 'Settings',
+    sref: 'settings'
+  }];
+
 });
