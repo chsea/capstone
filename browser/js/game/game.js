146c772629f97967f3859d8243503a8fd7d7a59f
Kate <katemcdonald39@gmail.com>
2015-10-17 19:20:43 -0400

    refactored description generator

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -54,52 +54,64 @@ app.config($stateProvider => {
       $scope.enlargedDescription = "this minion has no special powers";
     } else if (card.description){
       $scope.enlargedDescription = card.name + " " + card.description;
-    } else {
-      var desc = card.name + " has ";
-      var hasAbility = false;
-      
-      for (var key1 in card.logic) {
-        if (card.logic[key1] === true){
-          hasAbility = true;
-          desc += key1 + ", ";
+    } else { 
+      var abilityList = [];
+      var powerList = [];
+
+      for (var ability in card.logic){
+        if (card.logic[ability] === true){
+          abilityList.push(ability);
         }
       }
 
-      for (var key in card.logic){
-        if (typeof(card.logic[key]) === "object"){
-          hasAbility = true;
-          desc += key + ". It ";
-          switch (key) {
+      for (var power in card.logic) {
+        if (typeof(card.logic[power]) === "object") {
+          var str = card.name + " has " + power + ". It ";
+          switch (power) {
             case "charge":
-              desc += chargeInfo + ". ";
+              str += chargeInfo;
               break;
             case "taunt":
-              desc += tauntInfo + ". ";
+              str += tauntInfo;
               break;
             case "divineShield":
-              desc += divineShieldInfo + ". ";
+              str += divineShieldInfo;
               break;
             case "deathRattle":
-              desc += deathRattleInfo + ". ";
+              str += deathRattleInfo;
               break;
             case "windfury":
-              desc += windfuryInfo + ". ";
+              str += windfuryInfo;
               break;
             case "enrage":
-              desc += enrageInfo + ". ";
+              str += enrageInfo;
               break;
             case "battlecry":
-              desc += battlecryInfo + ". ";
+              str += battlecryInfo;
               break;
             default:
               break;
           }
+          powerList.push(str);
         }
       }
-      // cleanDesc removes trailing whitespace and commas
-      var cleanDesc = desc.charAt(-2) === "," ? desc.slice(0,-2) : desc.slice(0,-1);
-      $scope.enlargedDescription = hasAbility ? cleanDesc : "This minion has no special powers.";
-    }
+      var description = "";
+      if (abilityList.length === 1){
+        description = card.name + " has " + abilityList[0] + ". ";
+      }
+      if (abilityList.length > 1){
+        description = card.name + " has " + abilityList.join(", ") + ". ";
+      }
+      if (powerList.length){
+        description += powerList.join(". ");
+      }
+
+      if (abilityList.length || powerList.length){
+        $scope.enlargedDescription = description;
+      } else {
+        $scope.enlargedDescription = "This minion has no special powers.";
+      }
+    };
   };
 
 
